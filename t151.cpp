502a3e41b3a0a68e20dce14bff7e75e6ddfb1f43
nickmxxx <mxxxnick@bupt.edu.cn>
2023-03-06 14:08:08 +0800

    t151

--- t151.cpp
+++ t151.cpp
@@ -1,10 +1,16 @@
 #include<iostream>
 #include<vector>
 using namespace std;
+void reverse_words(string &s,int start,int end) {
+    for(int i=start,j=end;i<j;i++,j--) {
+        swap(s[i],s[j]);
+    }
+}
 int main(int argc, char const *argv[])
 {
-    string s = " the    skxy is blue  ";
+    string s = " the    skxy is blue";
     string s1 = "";
+    string ans = "";
     vector<int> v1;
     int length = s.length();
     for(int i=0;i<length;i++){
@@ -19,9 +25,25 @@ int main(int argc, char const *argv[])
         }
         // if(s[s.length()])
     }
-    s1  = s1.erase(0, s1.find_first_not_of(' ')); // 去掉前导空格
-    s1.erase(s1.find_last_not_of(' ') + 1); // 去掉后缀空格;
+    if(s1[0]==' '){
+        s1.erase(0,1);
+    }
+    if(s1[s1.length()-1]==' '){
+        s1.erase(s1.length()-1,1);
+    }
+    reverse_words(s1,0,s1.length()-1);
+    int start = 0;
+    for (int i = 0; i <= s1.length(); i++)
+    {
+        if(s1[i]==' '||i==s1.length()){
+            reverse_words(s1,start,i-1);
+            start = i+1;
+        }
+    }
+    
     cout<<s1;
+
+    
     // // return s1;
 
     return 0;
